425a4e2a1690737f48abaa92629f0a5a490742e4
xx <690815662@qq.com>
2016-10-13 21:05:46 +0800

    这是第一次测试

--- src/main/java/com/itheima/redboyclient/MainActivity.java
+++ src/main/java/com/itheima/redboyclient/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //这是第一次提 交
+        //这是第一次 提 交
     }
 }
